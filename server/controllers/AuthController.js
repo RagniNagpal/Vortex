7a8e1eb849e133d58aae002b077c59c2d7a20b47
RagniNagpal <ragninagpal@gmail.com>
2025-11-16 00:30:13 +0530

    backend part

---
+++ server/controllers/AuthController.js
@@ -0,0 +1,22 @@
+import { response } from "express";
+import User from "../models/UserModel";
+import {sign} from "jsonwebtoken"
+
+const maxAge=3*24*60*60*1000;
+
+const createToken=(email,userId)=>{
+  return sign({email,userId},process.env.JWT_KEY,{expiresIn:maxAge});
+}
+
+export const signup=async(request,response,next)=>{
+  try{
+    const {email, password} =req.body;
+    if(!email || !password){
+      return response.status(400).send("Email and Password is required");
+    }
+  }
+  catch(error){
+    console.log({error});
+    return response.status(500).send("Internal Server Error");
+  }
+}
