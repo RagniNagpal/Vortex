8f60ec587a426ba3373a251527494ed79c819f7f
RagniNagpal <ragninagpal@gmail.com>
2025-12-06 22:16:54 +0530

    added chat index.jsx

--- vortex/src/pages/chat/index.jsx
+++ vortex/src/pages/chat/index.jsx
@@ -1,5 +1,20 @@
-const Chat = ()=>{
-  return <div>Chat</div>
+import { useAppStore } from "@/store";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+const Chat = () => {
+  const { userInfo } = useAppStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userInfo.profileSetup) {
+      toast("Please setup profile to continue.");
+      navigate("/profile");
+    }
+  }, [userInfo, navigate]);
+
+  return <div>Chat</div>;
 };
 
 export default Chat;
