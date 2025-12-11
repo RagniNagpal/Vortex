5771318da9d10728e03e6e293ef2af399c27bef8
RagniNagpal <ragninagpal@gmail.com>
2025-12-11 21:19:46 +0530

    completing <Avatar>

--- vortex/src/pages/profile/index.jsx
+++ vortex/src/pages/profile/index.jsx
@@ -1,5 +1,55 @@
-const Profile = ()=>{
-  return <div>Profile</div>
+import { useAppStore } from "@/store";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+
+const Profile = () => {
+  const navigate=useNavigate();
+  const {userInfo,setUserInfo}=useAppStore();
+  const [firstName,setFirstName]=useState("");
+  const [LastName,setLastName]=useState("");
+  const [image,setImage]=useState(null);
+  const [hovered,setHovered]=useState(false);
+  const [selectedColor,setSelectedColor]=useState(0);
+
+  const saveChanges=async()=>{};
+
+  return (
+    <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
+      <div className="flex flex-col gap-10 w-[80vw] nd:w-max">
+        <div>
+          <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer"  />
+          </div>
+          <div className="grid grid-cols-2">
+            <div className="h-full w-32 md:w-48 md:h-48 relative flex items-center justify-center" 
+            onMouseEnter={()=>setHovered(true)}
+            onMouseLeave={()=>setHovered(false)}>
+               <Avatar className="w-32 h-32 md:w-48 md:h-48 rounded-full overflow-hidden">
+              {image ? (
+                <AvatarImage
+                  src={image}
+                  alt="profile"
+                  className="object-cover w-full h-full bg-black"
+                />
+              )  : (
+                <div className={`uppercase h-32 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full ${getColor(selectedColor)}`}>
+                  {firstName ? firstName.split("").shift() : userInfo.email.split("").shift()}</div>
+              )}
+            </Avatar>
+            {
+              hovered && (
+                <div>
+                  image ? <FaTrast
+                  </div>
+              )
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
